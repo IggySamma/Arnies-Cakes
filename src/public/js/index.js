6af123329dfc36b33889bafe455727b8c92b8c92
IggySamma <ignasfb@yahoo.ie>
2024-03-28 23:38:34 +0000

    Carousel mobile working, need to fix images and jump to top

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,14 +1,13 @@
-const galleryCarousel = document.querySelector('#carouselContainer')
+/*const galleryCarousel = document.querySelector('#carouselContainer')*/
 const testimonialCarousel = document.querySelector('#testimonialsCarousel')
 
 const homeGallery = document.querySelectorAll(".carousel-button")
-let homeGalleryActive = galleryCarousel.querySelector(".carousel-item.active") 
+let homeGalleryActive = document.querySelector('#carouselContainer').querySelector(".carousel-item.active") 
 
-const gCarousel = new bootstrap.Carousel(galleryCarousel, {
+const gCarousel = new bootstrap.Carousel(document.querySelector('#carouselContainer'), {
   interval: 2000,
   touch: false
 })
-
 
 
 homeGallery.forEach(button => {
@@ -20,12 +19,28 @@ homeGallery.forEach(button => {
     })
 })
 
-
-
+/*
 const tCarousel = new bootstrap.Carousel(testimonialCarousel, {
   interval: 3000,
   touch:    true,
   pause:    "hover",
   ride:     true,
   wrap:     true
-})
+})*/
+/*
+const galleryHref = document.querySelectorAll(".carousel-button")
+var carouselMobileJumpTo = window.matchMedia("(max-width: 992px)")
+carouselMobileJumpTo.addEventListener("change", function(){updateMobileCarousel(carouselMobileJumpTo)})
+
+function updateMobileCarousel(width){
+  if(width.matches){
+    galleryHref.forEach(button => {
+      button.href = "#carousel-items"
+    })
+  } else {
+    galleryHref.forEach(button => {
+      button.href = ""
+    })
+  }
+}
+*/
