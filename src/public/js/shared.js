ae76743fa7185b1860fc58e45951e9ecbea5fb73
IggySamma <ignasfb@yahoo.ie>
2024-05-24 23:06:15 +0100

    Footer finished and refactored shared js file for easier build step and read ability

--- src/public/js/shared.js
+++ src/public/js/shared.js
@@ -1,196 +1,181 @@
-function sideScroll() {
-    const attach = document.getElementById("sideScroll");
-    const container = document.createElement('div');
-    const row = document.createElement('div');
-    const colContain = document.createElement('div');
-    const colContainTwo = document.createElement('div');
-
-    const rowL = document.createElement('div');
-    const rowR = document.createElement('div');
-
-    attach.className = ("row sideScrollContainer");
-    container.className = ("col homeImageContainer");
-    attach.appendChild(container);
-    rowL.className = ("row sideScrollL m-1 d-inline-block")
-    container.appendChild(rowL);
-    rowR.className = ("row sideScrollR m-1 d-inline-block")
-    container.appendChild(rowR);
-
-    for(let j = 0; j <=1;j++){
-        for (let i = 1; i <= 3; i++) {
-            const col = document.createElement('div');
-            const img = document.createElement('img');
-
-            col.className = ("col p-0 my-1 py-1");
-            rowL.appendChild(col);
-            img.className = ("homeImage");
-            img.setAttribute("loading","lazy")
-            img.src = ("./images/Sidescroll/" + i +".jpg")
-            col.appendChild(img);
-        }
-        colContainTwo.className = ("col d-flex flex-column");
-        row.appendChild(colContainTwo);
-        for (let i = 4; i <= 6; i++) {
-            const col = document.createElement('div');
-            const img = document.createElement('img');
-
-            col.className = ("col p-0 my-1 py-1");
-            rowR.appendChild(col);
-            img.className = ("homeImage");
-            img.setAttribute("loading","lazy")
-            img.src = ("./images/Sidescroll/" + i +".jpg")
-            col.appendChild(img);
-        }  
-    } 
+function createElement(type, attributes = {}, classes = "", innerHTML = "") {
+  const element = document.createElement(type);
+  if (classes) element.className = classes;
+  for (let key in attributes) {
+      element.setAttribute(key, attributes[key]);
+  }
+  if (innerHTML) element.innerHTML = innerHTML;
+  return element;
 }
 
-function navBar(){
-    const attach = document.getElementById("navbar");
-    const containerFluid = document.createElement("div");
-    /*const hr = document.createElement("hr");*/
-    const toggleButton = document.createElement("button");
-    const spanIcon = document.createElement("span")
-    const mainDiv = document.createElement("div");
-    const dropDiv = document.createElement("div");
-    const ul = document.createElement("ul");
-    const li1 = document.createElement("li");
-    const li2 = document.createElement("li");
-    const li3 = document.createElement("li");
-    const li4 = document.createElement("li");
-    const li5 = document.createElement("li");
-    const li6 = document.createElement("li");
-    const li7 = document.createElement("li");
-
-    const a1 = document.createElement("a");
-    const a2 = document.createElement("a");
-    const a3 = document.createElement("a");
-    const a4 = document.createElement("a");
-    const a5 = document.createElement("a");
-    const a6 = document.createElement("a");
-    const a7 = document.createElement("a");
-    const a8 = document.createElement("a");
-    const a9 = document.createElement("a");
-    const a10 = document.createElement("a");
-    const a11 = document.createElement("a");
-    const a12 = document.createElement("a");
-    const a13 = document.createElement("a");
-
-    const logoContain = document.createElement("img");
-    const imgContain = document.createElement("img");
- 
-    attach.className = ("navbar fixed-top navbar-expand-lg");
-
-    containerFluid.className = ("container-fluid m-0 p-0 navContainer");
-    attach.appendChild(containerFluid)
-
-    toggleButton.className = ("navbar-toggler collapsed");
-    toggleButton.setAttribute("type", "button");
-    toggleButton.setAttribute("id", "toggling");
-    toggleButton.setAttribute("data-bs-toggle", "collapse");
-    toggleButton.setAttribute("data-bs-target", "#navbarSupportedContent");
-    spanIcon.className = ("navbar-toggler-icon")
-    toggleButton.appendChild(spanIcon);
- 
-    containerFluid.appendChild(toggleButton);
- 
-    mainDiv.className = ("collapse navbar-collapse justify-content-center");
-    mainDiv.setAttribute("id", "navbarSupportedContent");
-    mainDiv.style.top = "0";
-    containerFluid.appendChild(mainDiv);
- 
-    ul.className = ("navbar-nav");
-    mainDiv.appendChild(ul);
-    //Logo
-    li5.className = ("nav-item nav-logo");
-    ul.appendChild(li5);
-    a13.setAttribute('href', './Index.html')
-    li5.appendChild(a13)
-    logoContain.className = ('navBarLogo');
-    logoContain.setAttribute('src', './images/home logo.png');
-    a13.appendChild(logoContain);
-    //Home
-    li1.className = ("nav-item");
-    ul.appendChild(li1);
-    a1.className = ("nav-link active fontsize");
-    a1.setAttribute("href", "./Index.html");
-    a1.innerHTML = ("Home");
-    li1.appendChild(a1);
-    //About Me
-    li6.className = ("nav-item");
-    ul.appendChild(li6);
-    a11.className = ("nav-link fontsize");
-    a11.setAttribute("href", "./About.html");
-    a11.innerHTML = ("About Me");
-    li6.appendChild(a11);
-    //Gallery Dropdown
-    li2.className = ("nav-item dropdown");
-    ul.appendChild(li2);
-    a2.className = ("nav-link dropdown-toggle fontsize");
-    a2.setAttribute("href", "#");
-    a2.setAttribute("id", "navbarDropdown");
-    a2.setAttribute("role", "button");
-    a2.setAttribute("data-bs-toggle", "dropdown");
-    a2.innerHTML = ("Gallery");
-    li2.appendChild(a2);
-    //Gallery items
-    dropDiv.className = ("dropdown-menu fontsize");
-    li2.appendChild(dropDiv);
-
-    a3.className = ("dropdown-item");
-    a3.setAttribute("href", "./Gallery.html?type=All");
-    a3.innerHTML = ("All");
-    dropDiv.appendChild(a3);
-
-    a4.className = ("dropdown-item");
-    a4.setAttribute("href", "./Gallery.html?type=Cakes");
-    a4.innerHTML = ("Cakes");
-    dropDiv.appendChild(a4);
-
-    a5.className = ("dropdown-item");
-    a5.setAttribute("href", "./Gallery.html?type=Cakepops");
-    a5.innerHTML = ("Cakepops");
-    dropDiv.appendChild(a5);
-
-    a6.className = ("dropdown-item");
-    a6.setAttribute("href", "./Gallery.html?type=Cupcakes");
-    a6.innerHTML = ("Cupcakes");
-    dropDiv.appendChild(a6);
-
-    a7.className = ("dropdown-item");
-    a7.setAttribute("href", "./Gallery.html?type=KidsCakes");
-    a7.innerHTML = ("KidsCakes");
-    dropDiv.appendChild(a7);
-
-    a8.className = ("dropdown-item");
-    a8.setAttribute("href", "./Gallery.html?type=Treats");
-    a8.innerHTML = ("Treats");
-    dropDiv.appendChild(a8);
-    //Contact ME
-    li7.className = ("nav-item");
-    ul.appendChild(li7);
-    a12.className = ("nav-link fontsize");
-    a12.setAttribute("href", "./Contact.html");
-    a12.innerHTML = ("Contact Me");
-    li7.appendChild(a12);
-    //Flavours
-    li3.className = ("nav-item");
-    ul.appendChild(li3);
-    a9.className = ("nav-link fontsize");
-    a9.setAttribute("href", "./Flavours.html");
-    a9.innerHTML = ("Flavours");
-    li3.appendChild(a9);
-    //Enquires
-    li4.className = ("nav-item");
-    ul.appendChild(li4);
-    imgContain.className = ("ribbon");
-    imgContain.setAttribute("src", "./images/ribbon.png");
-    li4.appendChild(imgContain);
-    a10.className = ("nav-link fontsize");
-    a10.setAttribute("href", "./Enquiries.html");
-    a10.innerHTML = ("Enquiries");
-    li4.appendChild(a10);
+function appendChildren(parent, children) {
+  children.forEach(child => parent.appendChild(child));
 }
 
+/*Side Scroll*/
+function createImageColumn(src) {
+  const col = createElement('div', {}, "col p-0 my-1 py-1");
+  const img = createElement('img', { src, loading: "lazy" }, "homeImage");
+  col.appendChild(img);
+  return col;
+}
+
+function buildSideScroll() {
+  const attach = document.getElementById("sideScroll");
+  attach.className = "row sideScrollContainer m-0 p-0";
+
+  const container = createElement('div', {}, "col homeImageContainer");
+  const rowL = createElement('div', {}, "row sideScrollL m-1 d-inline-block");
+  const rowR = createElement('div', {}, "row sideScrollR m-1 d-inline-block");
+
+  attach.appendChild(container);
+  appendChildren(container, [rowL, rowR]);
+
+  for (let j = 0; j <= 1; j++) {
+      for (let i = 1; i <= 3; i++) {
+          rowL.appendChild(createImageColumn(`./images/Sidescroll/${i}.jpg`));
+      }
+      for (let i = 4; i <= 6; i++) {
+          rowR.appendChild(createImageColumn(`./images/Sidescroll/${i}.jpg`));
+      }
+  }
+}
+
+function buildNavBar() {
+  const attach = document.getElementById("navbar");
+  attach.className = "navbar fixed-top navbar-expand-lg";
+
+  const containerFluid = createElement("div", {}, "container-fluid m-0 p-0 navContainer");
+  attach.appendChild(containerFluid);
+
+  const toggleButton = createElement("button", {
+      type: "button",
+      id: "toggling",
+      "data-bs-toggle": "collapse",
+      "data-bs-target": "#navbarSupportedContent"
+  }, "navbar-toggler collapsed");
+  const spanIcon = createElement("span", {}, "navbar-toggler-icon");
+  toggleButton.appendChild(spanIcon);
+  containerFluid.appendChild(toggleButton);
+
+  const mainDiv = createElement("div", { id: "navbarSupportedContent" }, "collapse navbar-collapse justify-content-center");
+  mainDiv.style.top = "0";
+  containerFluid.appendChild(mainDiv);
+
+  const ul = createElement("ul", {}, "navbar-nav");
+  mainDiv.appendChild(ul);
+
+  // Define the nav items and links
+  const navItems = [
+      { class: "nav-item", linkClass: "nav-link active fontsize", href: "./Index.html", text: "Home" },
+      { class: "nav-item", linkClass: "nav-link fontsize", href: "./About.html", text: "About Me" },
+      { class: "nav-item dropdown", linkClass: "nav-link dropdown-toggle fontsize", href: "#", id: "navbarDropdown", role: "button", "data-bs-toggle": "dropdown", text: "Gallery" },
+      { class: "nav-item", linkClass: "nav-link fontsize", href: "./Contact.html", text: "Contact Me" },
+      { class: "nav-item", linkClass: "nav-link fontsize", href: "./Flavours.html", text: "Flavours" },
+      { class: "nav-item", linkClass: "nav-link fontsize", href: "./Enquiries.html", text: "Enquiries", extra: createElement("img", { src: "./images/ribbon.png" }, "ribbon") }
+  ];
+
+  // Add the logo item separately
+  const logoItem = createElement("li", {}, "nav-item nav-logo m-0 p-0");
+  const logoLink = createElement("a", { href: "./Index.html" });
+  const logoImg = createElement("img", { src: "./images/home logo.png" }, "navBarLogo");
+  logoLink.appendChild(logoImg);
+  logoItem.appendChild(logoLink);
+  ul.appendChild(logoItem);
+
+  // Append nav items to ul
+  navItems.forEach(item => {
+      const li = createElement("li", {}, item.class);
+      const a = createElement("a", { href: item.href, id: item.id, role: item.role, "data-bs-toggle": item["data-bs-toggle"] }, item.linkClass, item.text);
+      if (item.extra) li.appendChild(item.extra);
+      li.appendChild(a);
+      ul.appendChild(li);
+
+      if (item.text === "Gallery") {
+          const dropDiv = createElement("div", {}, "dropdown-menu fontsize");
+          const galleryItems = [
+              { href: "./Gallery.html?type=All", text: "All" },
+              { href: "./Gallery.html?type=Cakes", text: "Cakes" },
+              { href: "./Gallery.html?type=Cakepops", text: "Cakepops" },
+              { href: "./Gallery.html?type=Cupcakes", text: "Cupcakes" },
+              { href: "./Gallery.html?type=KidsCakes", text: "KidsCakes" },
+              { href: "./Gallery.html?type=Treats", text: "Treats" }
+          ];
+          galleryItems.forEach(gItem => {
+              const a = createElement("a", { href: gItem.href }, "dropdown-item", gItem.text);
+              dropDiv.appendChild(a);
+          });
+          li.appendChild(dropDiv);
+      }
+  });
+}
+
+function buildFooter() {
+  const attach = document.getElementById("footer");
+  attach.className = "footer container-fluid m-0 p-0";
+
+  const ul = createElement("ul", {}, "nav border-top m-0 p-0 footerList");
+  attach.appendChild(ul);
+
+  const logoItem = createElement("li", {}, "nav-item nav-logo");
+  const logoLink = createElement("a", { href: "./Index.html" });
+  const logoImg = createElement("img", { src: "./images/home logo.png" }, "footerLogo");
+  logoLink.appendChild(logoImg);
+  logoItem.appendChild(logoLink);
+  ul.appendChild(logoItem);
+
+  const footerItems = [
+      {
+          class: "nav-item my-auto",
+          link: [
+              { href: "https://www.instagram.com/arnies_cakes/", target: "_blank", img: { src: "./images/instagram.svg", class: "footerImages" } },
+              { href: "https://www.instagram.com/arnies_cakes/", target: "_blank", text: "@arnies_cakes", class: "footerLinks" }
+          ]
+      },
+      {
+          class: "nav-item my-auto",
+          link: [
+              { href: "https://www.facebook.com/ArniesCakes", target: "_blank", img: { src: "./images/facebook.svg", class: "footerImages" } },
+              { href: "https://www.facebook.com/ArniesCakes", target: "_blank", text: "Dublin Tortai - 'Arnie's Cakes'", class: "footerLinks" }
+          ]
+      },
+      {
+          class: "nav-item my-auto",
+          link: [
+              { href: "mailto:arniescakes@gmail.com", img: { src: "./images/gmail.svg", class: "footerImages" } },
+              { href: "mailto:arniescakes@gmail.com", text: "arniescakes@gmail.com", class: "footerLinks" }
+          ]
+      },
+      {
+          class: "nav-item footerEnquires my-auto rounded-5",
+          link: { href: "./Enquiries.html", text: "Enquires" }
+      }
+  ];
+
+  footerItems.forEach(item => {
+      const li = createElement("li", {}, item.class);
+      if (Array.isArray(item.link)) {
+          item.link.forEach(link => {
+              const a = createElement("a", { href: link.href, target: link.target || "" }, link.class || "", link.text || "");
+              if (link.img) {
+                  const img = createElement("img", { src: link.img.src }, link.img.class);
+                  a.appendChild(img);
+              }
+              li.appendChild(a);
+          });
+      } else {
+          const a = createElement("a", { href: item.link.href }, "", item.link.text || "");
+          if (item.link.img) {
+              const img = createElement("img", { src: item.link.img.src }, item.link.img.class);
+              a.appendChild(img);
+          }
+          li.appendChild(a);
+      }
+      ul.appendChild(li);
+  });
+}
+
+/*Nav disappear/appear on scroll*/
 var prevScrollpos = window.scrollY;
 var navCollapse = new bootstrap.Collapse(document.getElementById("navbar"))
 window.onscroll = function() {
