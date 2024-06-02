42fc9dfef254000f8d4f202e2b1486d66a78e340
IggySamma <ignasfb@yahoo.ie>
2024-06-03 00:51:34 +0100

    Container layout logic complete

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -1,6 +1,6 @@
 let urlParams = new URLSearchParams(window.location.search);
 let sectionName = urlParams.get('type');
-let container = document.getElementById("galleryContainer");
+let domContainer = document.getElementById("galleryContainer");
 
 function getGallery(){
   let data = {sectionName};
@@ -17,9 +17,16 @@ function getGallery(){
     .then(res => {
       return new Promise((resolve) =>{
         //console.log(res.data)
+        //console.log(res.data.length)
+        //console.log("required: " + res.data.length)
+        buildCollection(res.data)
+        //console.log("required: " + res.data.length)
+        //console.log("built: " + collectionMemory.count)
+        //console.log("collection: " + {collectionMemory})
       }) 
     }))
 }
+
 
 function createGalleryElement(type, attributes = {}, classes = "") {
   const element = document.createElement(type);
@@ -58,17 +65,155 @@ let widthCollection = {
     3:[w(3),w(3),w(2),w(2),w(2)]
 }
 
-function randomSelect(max){
-  return Math.floor(Math.random() * max);
+//console.log("width length " + (Object.keys(widthCollection).length - 1))
+
+function randomSelect(min, max){
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log(Object.keys(widthCollection[2]).length)
-console.log(widthCollection[2][randomSelect(4)])
+//console.log(Object.keys(widthCollection[2]).length)
 
-function randomWidth(collection){
-  let array = Object.values
-  for (let i = 0; i < array.length; i++){
+//collectionMemory mutates and reduces as used
+let collectionMemory = {
+  required: null,
+  count: 0,
+  previous: null,
+  current: null,
+  containers: [],
+  gallery: null
+}
+
+//collectionMemory[0] = randomSelect(1, 4)
+
+//console.log(widthCollection[0].length)
+//console.log(randomSelect(1, 3))
+
+function collectionCheck(){
+  if(collectionMemory.count === collectionMemory.required){
+    return true
+  } else if(collectionMemory.count > collectionMemory.required){
+    return true
+  } else {
+    return false
+  }
+}
+
+function collectionIncrease(bool){
+  if(bool === true){
+    collectionMemory.previous = collectionMemory.current
+    //console.log("Before Collection Increase + : " + collectionMemory.current)
+    collectionMemory.current += 1
+    //console.log("Collection Increase + : " + collectionMemory.current)
+    collectionMemory.count += widthCollection[collectionMemory.current].length
+    collectionMemory.containers.push(widthCollection[collectionMemory.current])
     return
+
+  } else {
+    collectionMemory.previous = collectionMemory.current
+    //console.log("Before Collection Increase - : " + collectionMemory.current)
+    collectionMemory.current -= 1
+    //console.log("Collection Increase - : " + collectionMemory.current)
+    collectionMemory.count += widthCollection[collectionMemory.current].length
+    collectionMemory.containers.push(widthCollection[collectionMemory.current])
+    return
+  }
+}
+
+function buildCollection(data){
+  if(collectionCheck() === true){
+    collectionMemory = {
+      containers: collectionMemory.containers,
+      gallery: data
+    }
+    console.log(collectionMemory)
+    insertImages()
+    return
+  }
+  if(collectionMemory.required === null){
+    if(data.length !== 0){
+      collectionMemory.required = data.length
+      collectionMemory.gallery = data
+      return buildCollection(collectionMemory.gallery)
+    } else {
+      //console.log("Api fetch error, no data")
+      return 504
+    }
+  }
+
+  if(collectionMemory.previous === null){
+    collectionMemory.previous = randomSelect(0,4)
+    collectionMemory.count += widthCollection[collectionMemory.previous].length
+    collectionMemory.containers.push(widthCollection[collectionMemory.previous])
+    //console.log("Previous initial: " + collectionMemory.previous)
+    return buildCollection(collectionMemory.gallery)
+  }
+
+  if(collectionMemory.current === null){
+    if(collectionMemory.previous === 0){
+      collectionMemory.current = 1
+      collectionMemory.containers.push(widthCollection[collectionMemory.current])
+      //console.log("Current initial: " + collectionMemory.current)
+      return buildCollection(collectionMemory.gallery)
+    }
+  
+    else if(collectionMemory.previous === (Object.keys(widthCollection).length - 1)){
+      collectionMemory.current = (Object.keys(widthCollection).length - 2)
+      collectionMemory.containers.push(widthCollection[collectionMemory.current]) 
+      //console.log("Current initial: " + collectionMemory.current)
+      return buildCollection(collectionMemory.gallery)
+    }
+
+    else {
+      if(randomSelect(0,1) === 1){
+        collectionMemory.current = (collectionMemory.previous + 1)
+        collectionMemory.containers.push(widthCollection[collectionMemory.current]) 
+        //console.log("Current initial from random +: " + collectionMemory.current)
+        return buildCollection(collectionMemory.gallery)
+      } else {
+        collectionMemory.current = (collectionMemory.previous - 1)
+        collectionMemory.containers.push(widthCollection[collectionMemory.current]) 
+        //console.log("Current initial from random -: " + collectionMemory.current)
+        return buildCollection(collectionMemory.gallery)
+      }
+    }
+  }
+
+  if(collectionMemory.current === 0){
+    //console.log(" = 0")
+    collectionIncrease(true)
+    return buildCollection(collectionMemory.gallery)
+  }
+
+  if(collectionMemory.current === (Object.keys(widthCollection).length - 1)){
+    //console.log(" = end")
+    collectionIncrease(false)
+    return buildCollection(collectionMemory.gallery)
+  }
+
+  if(collectionMemory.current > collectionMemory.previous){
+    //console.log(" current " + collectionMemory.current + " > previous " + collectionMemory.previous)
+    collectionIncrease(true)
+    return buildCollection(collectionMemory.gallery)
+  }
+
+  if(collectionMemory.current < collectionMemory.previous){
+    //console.log(" current " + collectionMemory.current + " < previous " + collectionMemory.previous)
+    collectionIncrease(false)
+    return buildCollection(collectionMemory.gallery)
+  }
+
+  console.log("Something went wrong in logic")
+  console.log(collectionMemory)
+  return 504
+}
+
+function insertImages(){
+  //console.log(collectionMemory.gallery[0].Path)
+  for(let i = collectionMemory.gallery.length -1; i > 0; i--){
+    const imageContainer = createGalleryElement('div',{style: "width: 30%"},"imageWrapper m-0 p-1")
+    domContainer.appendChild(imageContainer)
+    const image = createGalleryElement('img',{src:collectionMemory.gallery[i].Path},"img-thumbnail")
+    imageContainer.appendChild(image)
   }
 }
 
@@ -98,7 +243,8 @@ function randomWidth(collection){
 
 
 
-
+/*temp checks of creation will be remade for just dropping in object and data from fetch */
+/*
 const temp = createGalleryElement('div',{},"col-lg-3 imageWrapper m-0 p-1")
 container.appendChild(temp)
 
@@ -146,3 +292,4 @@ container.appendChild(temp17)
 
 const temp18 = createGalleryElement('img',{src:"/gallery/0484592647d0441112d7b1529b6d3905"},"img-thumbnail")
 temp17.appendChild(temp18)
+*/
