3e434413048f5537f5a91b839dc4a5a2e80024ae
IggySamma <ignasfb@yahoo.ie>
2024-06-08 02:49:42 +0100

    gallery layout actually done ?

--- src/public/js/gallery.js
+++ src/public/js/gallery.js
@@ -2,6 +2,7 @@ let urlParams = new URLSearchParams(window.location.search);
 let sectionName = urlParams.get('type');
 let domContainer = document.getElementById("galleryContainer");
 let galleryContainer = document.getElementsByClassName("galleryWrapper")
+let dimLength = 0;
 
 function getGallery(){
   let data = {sectionName};
@@ -34,28 +35,85 @@ function createGalleryElement(type, attributes = {}, classes = "") {
 
 
 function insertImages(data){
-  let counter = 0
+  let counter = 0;
+
   for(let i = 0; i < data.length; i++){
-    if(galleryContainer[counter].childNodes.length === 1){
-      const imageContainer = createGalleryElement(
+    /*if(galleryContainer[counter].childNodes.length === 1){
+      /*const imageContainer = createGalleryElement(
         'div', {loading: "lazy"}
-        ,"imageWrapper Type:" + data[i].Type + " ID:" + data[i].ID + " h" + counter + " m-0")
-      galleryContainer[counter].appendChild(imageContainer)
-      const image = createGalleryElement('img',{src:data[i].Path},"img")
-      imageContainer.appendChild(image)
+        ,"imageWrapper Type:" + data[i].Type + " ID:" + data[i].ID + " " + dimensions[dimLength] + /*" h" + counter + *//*" m-0");
+      galleryContainer[counter].appendChild(imageContainer);*/
+      /*const image = createGalleryElement('img',{src:data[i].Path, loading: "lazy"},"img" + " " + dimensions[dimLength] + " m-0");
+      //imageContainer.appendChild(image);
+      galleryContainer[counter].appendChild(image);
+      } else {
+      /*const imageContainer = createGalleryElement(
+        'div', {loading: "lazy"}
+        ,"imageWrapper Type:" + data[i].Type + " ID:" + data[i].ID + " " + dimensions[dimLength] + /*" h" + randomInt(0, 6) +*/ /*" m-0");
+      galleryContainer[counter].appendChild(imageContainer);*/
+      /*const image = createGalleryElement('img',{src:data[i].Path, loading: "lazy"},"img" + " " + dimensions[dimLength] + " m-0");
+      //imageContainer.appendChild(image);
+      galleryContainer[counter].appendChild(image);
+    }
+*/
+
+      /*const imageContainer = createGalleryElement(
+        'div', {loading: "lazy"}
+        ,"imageWrapper Type:" + data[i].Type + " ID:" + data[i].ID + " " + dimensions[dimLength] + " m-0");
+      /*galleryContainer[counter].appendChild(imageContainer);
+      const image = createGalleryElement('img',{src:data[i].Path, loading: "lazy"},"img"/* + " " + dimensions[dimLength] + " m-0");
+      //imageContainer.appendChild(image);
+      imageContainer.appendChild(image);*/
+    if(dimensions[dimLength].includes("w52")){
+      let imageContainer = createGalleryElement(
+        'div', {}
+        ,"imageWrapper " + dimensions[dimLength].replace("hh0 w52", "h0") + " m-0 p-0");
+      galleryContainer[counter].appendChild(imageContainer);
+      const image = createGalleryElement('img',{src:data[i].Path, loading: "lazy"},
+        "img" + " Type:" + data[i].Type + " ID:" + data[i].ID  + " " + dimensions[dimLength] + " m-0 p-1");
+      imageContainer.appendChild(image);
+    } else if(dimensions[dimLength].includes("w53")){
+      let imageContainer = document.getElementsByClassName("imageWrapper")
+      console.log(imageContainer[imageContainer.length-1])
+      const image = createGalleryElement('img',{src:data[i].Path, loading: "lazy"},
+        "img" + " Type:" + data[i].Type + " ID:" + data[i].ID  + " " + dimensions[dimLength] + " m-0 p-1");
+      imageContainer[imageContainer.length-1].appendChild(image);
     } else {
-      const imageContainer = createGalleryElement(
-        'div', {loading: "lazy"}
-        ,"imageWrapper Type:" + data[i].Type + " ID:" + data[i].ID + " m-0")
-      galleryContainer[counter].appendChild(imageContainer)
-      const image = createGalleryElement('img',{src:data[i].Path},"img")
-      imageContainer.appendChild(image)
+      const image = createGalleryElement('img',{src:data[i].Path, loading: "lazy"},
+        "img" + " Type:" + data[i].Type + " ID:" + data[i].ID  + " " + dimensions[dimLength] + " m-0 p-1");
+      galleryContainer[counter].appendChild(image);
     }
 
-    if(counter === 5){
-      counter = 0
+    if(counter === galleryContainer.length - 1){
+      counter = 0;
     } else {
-      counter = counter +1
+      counter = counter + 1;
+    }
+
+    if(dimLength === dimensions.length - 1){
+      dimLength = 0;
+    } else {
+      dimLength = dimLength + 1;
     }
   }
+  let widthCheck = document.getElementsByClassName("w51");
+  let wrapperCheck = document.getElementsByClassName("imageWrapper");
+  if(widthCheck.length > wrapperCheck.length){
+    widthCheck[widthCheck.length-1].style.width = "100%";
+  }
 }
+
+function randomInt(min, max){
+  return Math.floor(Math.random() * (max - min) + min); 
+}
+
+let dimensions = [
+  "h0", "h1",      "h2", "h3", "h4",
+  "h3", "h0 w51",  "h1", "h1", "h2",
+  "h1", "hh0 w52", "h4", "h1", "h3",
+  "h2", "hh0 w53", "h3", "h2", "h1",
+  "h4", "h0",      "h2", "h1", "h4",
+  "h3", "h4",      "h2", "h3", "h2" 
+]
+
+
