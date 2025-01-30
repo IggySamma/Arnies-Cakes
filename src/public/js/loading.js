ea1e3d58768d9bbb6fe4377286ca901cac7fe4eb
IggySamma <ignasfb@yahoo.ie>
2025-01-30 23:27:00 +0000

    Loading screen fixed, general tidy up on different formats

--- src/public/js/loading.js
+++ src/public/js/loading.js
@@ -1,9 +1,13 @@
 function show(){
-	update = document.querySelectorAll('*');
+	/*update = document.querySelectorAll('*');
 	hide = document.getElementById('loading')
 	update.forEach( element => {
 		element.style.visibility = 'visible'
 	})
 	hide.style.visibility = 'hidden';
-	hide.style.display = 'none';
+	hide.style.display = 'none';*/
+	update = document.querySelectorAll('#loading');
+	update.forEach(element => {
+		element.remove();
+	})
 }
